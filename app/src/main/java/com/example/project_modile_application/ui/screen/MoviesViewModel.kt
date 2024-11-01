f24e3fe07893a3d57a1c924be5c0d4e51320661f
nurbaanuarov <220103031@stu.sdu.edu.kz>
2024-11-01 12:12:23 +0500

    some additions on model view and changes on Home page

--- app/src/main/java/com/example/project_modile_application/ui/screen/MoviesViewModel.kt
+++ app/src/main/java/com/example/project_modile_application/ui/screen/MoviesViewModel.kt
@@ -1,4 +1,32 @@
 package com.example.project_modile_application.ui.screen
 
-class MoviesViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.project_modile_application.data.model.MoviesList
+import com.example.project_modile_application.data.network.MoviesApi
+import kotlinx.coroutines.launch
+
+sealed interface MoviesUiState {
+    data class Success(val moviesList: MoviesList) : MoviesUiState
+    object Error : MoviesUiState
+    object Loading : MoviesUiState
+}
+
+class MoviesViewModel : ViewModel() {
+    var moviesUiState: MoviesUiState by mutableStateOf(MoviesUiState.Loading)
+        private set
+
+    private fun getMoviesList() {
+        viewModelScope.launch {
+            try {
+                val listResult = MoviesApi.retrofitService.getMovieList()
+                moviesUiState = MoviesUiState.Success(listResult)
+            } catch (e: Exception) {
+                moviesUiState = MoviesUiState.Error
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/project_modile_application/ui/screen/home/Home.kt
+++ app/src/main/java/com/example/project_modile_application/ui/screen/home/Home.kt
@@ -12,37 +12,48 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.project_modile_application.R
-import com.example.project_modile_application.data.network.MoviesApiService
+import com.example.project_modile_application.ui.screen.MoviesUiState
 import com.example.project_modile_application.ui.screen.home.components.Category
 
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun HomePrev() {
-    Home()
+fun HomePrev(
+    moviesUiState: MoviesUiState,
+    modifier: Modifier = Modifier
+) {
+    Home(moviesUiState)
 }
 
 @Composable
-fun Home() {
-    val moviesApiService = MoviesApiService()
-    Column(Modifier.padding(start = 26.dp, end = 26.dp, top = 55.dp)) {
-        Image(
-            painter = painterResource(R.drawable.skillcinema),
-            contentDescription = "",
-            modifier = Modifier
-                .padding(bottom = 10.dp)
-        )
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .verticalScroll(rememberScrollState(0)),
-        ) {
-            Category("Премьеры", moviesApiService.loadPremieres())
-            Category("Популярное",moviesApiService.loadPopularMovies())
-            Category("Боевики США", moviesApiService.loadActionMovies())
-            Category("Драмы Франции", moviesApiService.loadDramaMovies())
-            Category("Сериалы", moviesApiService.loadSeries())
-        }
+fun Home(
+    moviesUiState: MoviesUiState,
+    modifier: Modifier = Modifier
+) {
+    when (moviesUiState) {
+        is MoviesUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
+        is MoviesUiState.Success -> ResultScreen(
+            Column(Modifier.padding(start = 26.dp, end = 26.dp, top = 55.dp)) {
+                Image(
+                    painter = painterResource(R.drawable.skillcinema),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(bottom = 10.dp)
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .verticalScroll(rememberScrollState(0)),
+                ) {
+                    Category("Премьеры", moviesApiService.loadPremieres())
+                    Category("Популярное",moviesApiService.loadPopularMovies())
+                    Category("Боевики США", moviesApiService.loadActionMovies())
+                    Category("Драмы Франции", moviesApiService.loadDramaMovies())
+                    Category("Сериалы", moviesApiService.loadSeries())
+                }
 
+            }
+        )
+
+        is MoviesUiState.Error -> ErrorScreen( modifier = modifier.fillMaxSize())
     }
 
 }
