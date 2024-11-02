38921bb34746e936a9e3ed302a1c4595bdbfc1a0
color.ui <eskendir16092004@gmail.com>
2024-11-02 13:12:09 +0500

    Separate to the packages and little changes

--- app/src/main/java/com/example/project_modile_application/presentation/MainActivity.kt
+++ app/src/main/java/com/example/project_modile_application/presentation/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.project_modile_application.presentation.navigation.MainComposable
-import com.example.project_modile_application.ui.theme.Project_Modile_ApplicationTheme
+import com.example.project_modile_application.presentation.ui.theme.Project_Modile_ApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
